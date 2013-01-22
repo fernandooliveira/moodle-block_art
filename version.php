b4995566e7708f8ea70b6a1e2cca7914beb8bb22
fernandooliveira <fernandooliveira@knet.ca>
2013-01-22 10:36:10 -0500

    first commit with latest code

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2013011700;
+$plugin->cron     = 0;
+$plugin->requires = 2010112400; // Moodle 2.0+
+$plugin->release  = '2.x (Build: 2012120700)';
+$plugin->maturity = MATURITY_ALPHA;
