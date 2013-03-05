82750195960aa22c66041cecca4b0eaa34348238
k-net <fernandooliveira@knet.ca>
2013-03-05 14:46:26 -0500

    Update version.php

--- version.php
+++ version.php
@@ -1,4 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+* Keeps track of the version number
+*
+* @package block
+* @subpackage fn_assignment_reset
+* @copyright 2013 MoodleFN
+* @author MoodleFN - www.moodlefn.knet.ca
+*/
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -6,4 +30,5 @@ $plugin->version  = 2013011700;
 $plugin->cron     = 0;
 $plugin->requires = 2010112400; // Moodle 2.0+
 $plugin->release  = '2.x (Build: 2012120700)';
+$plugin->component = 'block_fn_assignment_reset';
 $plugin->maturity = MATURITY_ALPHA;
